a409cc25c448c3182f8341a10e841072e6bac983
Solomon Xie <solomonxiewise@gmail.com>
2018-07-25 18:38:47 +0800

    init

---
+++ emailpy.py
@@ -0,0 +1,12 @@
+import poplib
+
+from email.parser import Parser
+from email.header import decode_header
+from email.utils import parseaddr
+
+
+
+class EmailPy:
+    def __init__(self):
+        pass
+
